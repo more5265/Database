62344b4e7d515c4c5ed6d405936ea068fff9de07
more5265 <more5265@gmail.com>
2023-06-21 17:21:48 +0900

    Database SQL 수업예제

--- 1_SQL 기초.sql
+++ 1_SQL 기초.sql
@@ -2,11 +2,16 @@
 #이름 : 이황성
 #내용 : 1.SQL 기본 실습
 
-#실습1-1
+# 자료형
+# SQL 데이터 형식은 크게 숫자, 문자, 날짜(시간) 데이터 형식 3가지로 나눔
+# TINYINT (매우작은정수, -128~127), INT (일반정수)
+# CHAR (고정형 문자열) VARCHAR (가변형 문자열)
+# DATE (날짜) YEAR (년도) TIME (시간) DATETIME (날짜와 시간)
+#실습1-1 데이터베이스 생성 및 제거
 CREATE DATABASE `UserDB`;
 DROP DATABASE `UserDB`;
 
-#실습1-2
+#실습1-2 테이블 생성 및 제거
 CREATE TABLE `User1` (
 	`uid`		VARCHAR(10),
 	`name`	VARCHAR(10),
@@ -16,7 +21,9 @@ CREATE TABLE `User1` (
 	
 DROP TABLE `TblUser`;
 
-#실습1-3
+#실습1-3 테이블에 데이터 추가
+# INSERT INTO `테이블명` VALUES (데이터1, 데이터2, ...);
+
 INSERT INTO `User1` VALUES ('A101', '김유신', '010-1234-1111', 25);
 INSERT INTO `User1` VALUES ('A102', '김춘추', '010-1234-2222', 23);
 INSERT INTO `User1` VALUES ('A103', '김유신', '010-1234-3333', 32);
@@ -24,20 +31,26 @@ INSERT INTO `User1` (`uid`, `name`, `age`) VALUES ('A104', '강감찬', '45');
 INSERT INTO `User1` SET `uid`='A105', `name`='이순신', `hp`='010-1234-5555';
 
 
-#실습1-4
+#실습1-4 테이블에 데이터 조회
+# 모든 데이터 조회 SELECT * FROM `테이블명`;
 SELECT * FROM `User1`;
 SELECT * FROM `User1` WHERE `uid`='A101';
 SELECT * FROM `User1` WHERE `name`='김춘추';
 SELECT * FROM `User1` WHERE `age` < 30;
 SELECT * FROM `User1` WHERE `age` >= 30;
+
+# SELECT `칼럼명`, `칼럼명2`, ... FROM `테이블명` WHERE 조건;
 SELECT `uid`, `name`, `age` FROM `User1`;
 
-#실습1-5
+#실습1-5 데이터 수정
+
+#조건에 일치하는 레코드만 수정
+# UPDATE `테이블명` SET `칼럼명`='데이터'.... WHERE 조건;
 UPDATE `User1` SET `hp`='010-1234-4444' WHERE `uid`='A104';
 UPDATE `User1` SET `age` = 51 WHERE `uid` = 'A105';
 UPDATE `User1` SET `hp` = '010-1234-1001', `age` = 27 WHERE `uid` = 'A101';
 
-#실습1-6
+#실습1-6 테이블에 데이터 삭제
 DELETE FROM `User1` WHERE `uid` = 'A101';
 DELETE FROM `User1` WHERE `uid` = 'A102' AND `age`=25;
 DELETE FROM `User1` WHERE `age` >= 30;
